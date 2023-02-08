5bf8615229333636812d760c2d3b74790a7d7aec
Luigi Oliveira <luigijordanio@hotmail.com>
2023-02-07 22:24:59 -0300

    Initial Commit

--- src/index.tsx
+++ src/index.tsx
@@ -30,7 +30,7 @@ createServer({
     })
   },
   routes(){
-    this.namespace = 'api';
+    this.namespace = 'api'; 
     
     this.get('/transactions', () => {
       return this.schema.all('transaction')
